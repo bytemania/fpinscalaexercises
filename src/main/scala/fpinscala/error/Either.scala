aa2ecd7d955883f0f8e0e0b4e202f2472fbc5e4f
silvaa1 <antonio.silva@paddypowerbetfair.com>
2020-06-09 14:35:05 +0100

    ADT

--- src/main/scala/fpinscala/error/Either.scala
+++ src/main/scala/fpinscala/error/Either.scala
@@ -5,25 +5,13 @@ package fpinscala.error {
      * Implement versions of map, flatMap, orElse, and map2 on Either that operate on the Right value.
      */
 
-    def map[B](f: A => B): Either[E, B] = this match {
-      case e @ Left(_) => e
-      case Right(v) => Right(f(v))
-    }
+    def map[B](f: A => B): Either[E, B] = ???
 
-    def flatMap[EE >: E,B](f: A => Either[EE, B]): Either[EE, B] = this match {
-      case e @ Left(_) => e
-      case Right(v) => f(v)
-    }
+    def flatMap[EE >: E,B](f: A => Either[EE, B]): Either[EE, B] = ???
 
-    def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
-      case Left(_) => b
-      case e @ Right(_) => e
-    }
+    def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
 
-    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = for {
-      x <- this
-      y <- b
-    } yield f(x,y)
+    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
   }
 
   object Either {
@@ -31,10 +19,9 @@ package fpinscala.error {
      * Implement sequence and traverse for Either. These should return the first error that’s encountered,
      * if there is one.
      */
-    def sequence[E,A](es: List[Either[E,A]]): Either[E, List[A]] = traverse(es)(identity)
+    def sequence[E,A](es: List[Either[E,A]]): Either[E, List[A]] = ???
 
-    def traverse[E,A,B](as: List[A])(f: A => Either[E,B]): Either[E, List[B]] =
-      as.foldRight[Either[E,List[B]]](Right(Nil))((a,acc) => f(a).map2(acc)(_ :: _))
+    def traverse[E,A,B](as: List[A])(f: A => Either[E,B]): Either[E, List[B]] = ???
 
   }
 
